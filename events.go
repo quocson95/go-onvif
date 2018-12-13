4de0c6990877b41658d29dcc519dfc73e0fc1674
Nguyen Van Phuoc <phuocpr1998@gmail.com>
2018-12-13 15:25:47 +0700

    add events

---
+++ events.go
@@ -0,0 +1,29 @@
+package onvif
+
+func (device Device) Subscribe(address string) (error) {
+	// create soap
+	soap := SOAP{
+		User: device.User,
+		Password: device.Password,
+		Body: `<Subscribe xmlns="http://docs.oasis-open.org/wsn/b-2">
+					<ConsumerReference>
+						<Address>` + address + `</Address>
+					</ConsumerReference>
+					<InitialTerminationTime>PT60S</InitialTerminationTime>
+			   </Subscribe>`,
+	}
+
+	// send request
+	response, err := soap.SendRequest(device.XAddr)
+	if err != nil{
+		return err
+	}
+
+	_, err = response.ValueForPath("Envelope.Body.SubscribeResponse")
+	if err != nil{
+		return err
+	}
+
+	return nil
+}
+
