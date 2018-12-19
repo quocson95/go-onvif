987d9bdc3c0a8dd125d655165ab0b91d0eba87bc
Nguyen Van Phuoc <phuocpr1998@gmail.com>
2018-12-19 16:44:46 +0700

    add UnSubscribe

--- events.go
+++ events.go
@@ -1,6 +1,8 @@
 package onvif
 
-func (device Device) Subscribe(address string, timeout string) (error) {
+
+// return url for unsubscribe
+func (device Device) Subscribe(address string) (string, error) {
 	// create soap
 	soap := SOAP{
 		User: device.User,
@@ -9,21 +11,42 @@ func (device Device) Subscribe(address string, timeout string) (error) {
 					<ConsumerReference>
 						<Address>` + address + `</Address>
 					</ConsumerReference>
-					<InitialTerminationTime>` + timeout +`</InitialTerminationTime>
-			   </Subscribe>`,
+				</Subscribe>`,
 	}
 
+	var result string = ""
 	// send request
 	response, err := soap.SendRequest(device.XAddr)
 	if err != nil{
-		return err
+		return result, err
 	}
 
-	_, err = response.ValueForPath("Envelope.Body.SubscribeResponse")
+	ifaceResult, err := response.ValueForPath("Envelope.Body.SubscribeResponse.SubscriptionReference.Address")
+	if err != nil{
+		return result, err
+	}
+
+	result,_ = ifaceResult.(string)
+	return result, nil
+}
+
+
+func (device Device) UnSubscribe(address string) error {
+	// create soap
+	soap := SOAP{
+		User: device.User,
+		Password: device.Password,
+		Body: `<Unsubscribe xmlns="http://docs.oasis-open.org/wsn/b-2"/>`,
+	}
+	// send request
+	response, err := soap.SendRequest(address)
+	if err != nil{
+		return err
+	}
+	_, err = response.ValueForPath("Envelope.Body.UnsubscribeResponse")
 	if err != nil{
 		return err
 	}
 
 	return nil
 }
-
