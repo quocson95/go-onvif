a3db3caac6d7b3dd13ffd5efe520cd721b94e5fa
Nguyễn Văn Phước <phuocpr1998@gmail.com>
2020-11-16 16:18:51 +0700

    Add some function for event namespace

--- events.go
+++ events.go
@@ -1,11 +1,10 @@
 package onvif
 
-
 // return url for unsubscribe
 func (device Device) Subscribe(address string) (string, error) {
 	// create soap
 	soap := SOAP{
-		User: device.User,
+		User:     device.User,
 		Password: device.Password,
 		Body: `<Subscribe xmlns="http://docs.oasis-open.org/wsn/b-2">
 					<ConsumerReference>
@@ -18,34 +17,144 @@ func (device Device) Subscribe(address string) (string, error) {
 	var result string = ""
 	// send request
 	response, err := soap.SendRequest(device.XAddr)
-	if err != nil{
+	if err != nil {
 		return result, err
 	}
 
 	ifaceResult, err := response.ValueForPath("Envelope.Body.SubscribeResponse.SubscriptionReference.Address")
-	if err != nil{
+	if err != nil {
 		return result, err
 	}
 
-	result,_ = ifaceResult.(string)
+	result, _ = ifaceResult.(string)
 	return result, nil
 }
 
+// return url for unsubscribe
+func (device Device) CreatePullPointSubscription() (CreatePullPointSubscriptionResponse, error) {
+	// create soap
+	soap := SOAP{
+		User:     device.User,
+		Password: device.Password,
+		Action:   "http://www.onvif.org/ver10/events/wsdl/EventPortType/CreatePullPointSubscriptionRequest",
+		Body: `<CreatePullPointSubscription xmlns="http://www.onvif.org/ver10/events/wsdl">
+					<InitialTerminationTime>PT3600S</InitialTerminationTime>
+				</CreatePullPointSubscription>`,
+	}
+
+	result := CreatePullPointSubscriptionResponse{}
+	// send request
+	response, err := soap.SendRequest(device.XAddr)
+	if err != nil {
+		return result, err
+	}
+
+	ifaceResult, err := response.ValueForPath("Envelope.Body.CreatePullPointSubscriptionResponse")
+	if err != nil {
+		return result, err
+	}
+
+	if mapMetadata, ok := ifaceResult.(map[string]interface{}); ok {
+		result.CurrentTime = interfaceToString(mapMetadata["CurrentTime"])
+		result.TerminationTime = interfaceToString(mapMetadata["TerminationTime"])
+		result.SubscriptionReference = SubscriptionReference{}
+		if mapSubscriptionReference, ok := mapMetadata["SubscriptionReference"].(map[string]interface{}); ok {
+			result.SubscriptionReference.Address = interfaceToString(mapSubscriptionReference["Address"])
+		}
+	}
+
+	return result, nil
+}
+
+// return url for unsubscribe
+func (device Device) GetEventProperties() (interface{}, error) {
+	// create soap
+	soap := SOAP{
+		User:     device.User,
+		Password: device.Password,
+		Body:     `<GetEventProperties xmlns="http://www.onvif.org/ver10/events/wsdl"/>`,
+	}
+
+	// send request
+	response, err := soap.SendRequest(device.XAddr)
+	if err != nil {
+		return "", err
+	}
+
+	ifaceResult, err := response.ValueForPath("Envelope.Body.GetEventPropertiesResponse")
+	if err != nil {
+		return "", err
+	}
+
+	return ifaceResult, nil
+}
+
+// return url for unsubscribe
+func (device Device) PullMessages(address string) ([]NotificationMessage, error) {
+	// create soap
+	soap := SOAP{
+		User:     device.User,
+		Password: device.Password,
+		Action:   "http://www.onvif.org/ver10/events/wsdl/PullPointSubscription/PullMessagesRequest",
+		Body: `<PullMessages xmlns="http://www.onvif.org/ver10/events/wsdl">
+					<Timeout>1</Timeout>
+					<MessageLimit>100</MessageLimit>
+				</PullMessages>`,
+	}
+
+	var result = make([]NotificationMessage, 0)
+	// send request
+	response, err := soap.SendRequest(address)
+	if err != nil {
+		return result, err
+	}
+
+	ifaceResult, err := response.ValuesForPath("Envelope.Body.PullMessagesResponse.NotificationMessage")
+	if err != nil {
+		return result, err
+	}
+
+	for _, notificationMessage := range ifaceResult {
+		if mapNotiMsg, ok := notificationMessage.(map[string]interface{}); ok {
+			msg := NotificationMessage{}
+			if mapTopic, ok := mapNotiMsg["Topic"].(map[string]interface{}); ok {
+				msg.Topic = interfaceToString(mapTopic["#text"])
+			}
+			if mapMsg, ok := mapNotiMsg["Message"].(map[string]interface{}); ok {
+				if mapMsg, ok := mapMsg["Message"].(map[string]interface{}); ok {
+					msg.UtcTime = interfaceToString(mapMsg["-UtcTime"])
+					if mapData, ok := mapMsg["Data"].(map[string]interface{}); ok {
+						if mapSimpleItem, ok := mapData["SimpleItem"].(map[string]interface{}); ok {
+							msg.Data = interfaceToString(mapSimpleItem["-Value"])
+						}
+					}
+					if mapSource, ok := mapMsg["Source"].(map[string]interface{}); ok {
+						if mapSimpleItem, ok := mapSource["SimpleItem"].(map[string]interface{}); ok {
+							msg.Source = interfaceToString(mapSimpleItem["-Value"])
+						}
+					}
+				}
+			}
+			result = append(result, msg)
+		}
+	}
+	return result, nil
+}
 
 func (device Device) UnSubscribe(address string) error {
 	// create soap
 	soap := SOAP{
-		User: device.User,
+		User:     device.User,
 		Password: device.Password,
-		Body: `<Unsubscribe xmlns="http://docs.oasis-open.org/wsn/b-2"/>`,
+		Body:     `<Unsubscribe xmlns="http://docs.oasis-open.org/wsn/b-2"/>`,
 	}
 	// send request
 	response, err := soap.SendRequest(address)
-	if err != nil{
+	if err != nil {
 		return err
 	}
 	_, err = response.ValueForPath("Envelope.Body.UnsubscribeResponse")
-	if err != nil{
+	if err != nil {
 		return err
 	}
 
--- model.go
+++ model.go
@@ -32,23 +32,23 @@ type NetworkCapabilities struct {
 
 // MediaCapabilities contains media capabilities of ONVIF camera
 type MediaCapabilities struct {
-	XAddr     string
+	XAddr string
 }
 
 type PTZCapabilities struct {
-	XAddr     string
+	XAddr string
 }
 
 type EventsCapabilities struct {
-	Events    map[string]bool
-	XAddr     string
+	Events map[string]bool
+	XAddr  string
 }
 
 // DeviceCapabilities contains capabilities of an ONVIF camera
 type DeviceCapabilities struct {
 	Network   NetworkCapabilities
-	Media	  MediaCapabilities
-	Ptz		  PTZCapabilities
+	Media     MediaCapabilities
+	Ptz       PTZCapabilities
 	EventsCap EventsCapabilities
 	Streaming map[string]bool
 }
@@ -82,7 +82,7 @@ type VideoRateControl struct {
 
 // VideoEncoderConfig contains configuration of a video encoder
 type H264Configuration struct {
-	GovLength int
+	GovLength   int
 	H264Profile string //'Baseline', 'Main', 'Extended', 'High'
 }
 
@@ -94,8 +94,8 @@ type VideoEncoderConfig struct {
 	RateControl    VideoRateControl
 	Resolution     MediaBounds
 	SessionTimeout string
-	H264 H264Configuration
-	Multicast Multicast
+	H264           H264Configuration
+	Multicast      Multicast
 }
 
 // AudioEncoderConfig contains configuration of an audio encoder
@@ -136,15 +136,15 @@ type MediaURI struct {
 
 // System Date And Time
 type Time struct {
-	Hour int
+	Hour   int
 	Minute int
 	Second int
 }
 
 type Date struct {
-	Year int
+	Year  int
 	Month int
-	Day int
+	Day   int
 }
 
 type UTCDateTime struct {
@@ -156,7 +156,7 @@ type TimeZone struct {
 }
 
 type SystemDateAndTime struct {
-	DateTimeType string
+	DateTimeType    string
 	DaylightSavings bool
 	TimeZone
 	UTCDateTime
@@ -164,91 +164,89 @@ type SystemDateAndTime struct {
 
 //NTP information struct
 type NetworkHost struct {
-	Type string
+	Type        string
 	IPv4Address string
-	DNSname string
+	DNSname     string
 }
 
-
 type NTPInformation struct {
-	FromDHCP bool
+	FromDHCP       bool
 	NTPNetworkHost NetworkHost // NetworkHost of NTPFromDHCP if FromDHCP is true, else of NTPManual
 }
 
 // DNS Information struct
 type IPAddress struct {
-	Type string
+	Type        string
 	IPv4Address string
 }
 
-
 type DNSInformation struct {
-	FromDHCP bool
+	FromDHCP     bool
 	SearchDomain string
-	DNSAddress IPAddress // IPAddress of DNSFromDHCP if FromDHCP is true, else of DNSManual
+	DNSAddress   IPAddress // IPAddress of DNSFromDHCP if FromDHCP is true, else of DNSManual
 }
+
 // DynamicDNSInformation
 type DynamicDNSInformation struct {
 	Type string // 'NoUpdate', 'ClientUpdates', 'ServerUpdates'
 	Name string
-	TTL string
+	TTL  string
 }
 
 // NetWork Interface
 type NetworkInterfaceInfo struct {
-	Name string
+	Name      string
 	HwAddress string
-	MTU int
+	MTU       int
 }
 
 type NetworkInterfaceConnectionSetting struct {
 	AutoNegotiation bool
-	Speed int
-	Duplex string // "Full" || "Half"
+	Speed           int
+	Duplex          string // "Full" || "Half"
 }
 
 type NetworkInterfaceLink struct {
 	AdminSettings NetworkInterfaceConnectionSetting
-	OperSettings NetworkInterfaceConnectionSetting
+	OperSettings  NetworkInterfaceConnectionSetting
 	InterfaceType string
 }
 
 type PrefixedIPAdress struct {
-	Address string
+	Address      string
 	PrefixLength int
 }
 
 type IPv4Configuration struct {
-	Manual PrefixedIPAdress
+	Manual    PrefixedIPAdress
 	LinkLocal PrefixedIPAdress
-	FromDHCP PrefixedIPAdress
-	DHCP bool
+	FromDHCP  PrefixedIPAdress
+	DHCP      bool
 }
 
 type IPv4NetworkInterface struct {
 	Enabled bool
-	Config IPv4Configuration
+	Config  IPv4Configuration
 }
 
-
 type NetworkInterface struct {
-	Token string
+	Token   string
 	Enabled bool
-	Info NetworkInterfaceInfo
-	Link NetworkInterfaceLink
-	IPv4 IPv4NetworkInterface
+	Info    NetworkInterfaceInfo
+	Link    NetworkInterfaceLink
+	IPv4    IPv4NetworkInterface
 }
 
 // NetWork Protocols
 type NetworkProtocol struct {
-	Name string // 'HTTP' 'HTTPS' 'RTSP'
+	Name    string // 'HTTP' 'HTTPS' 'RTSP'
 	Enabled bool
-	Port int
+	Port    int
 }
 
 // Scope
 type Scope struct {
-	ScopeDes string // 'Fixed' 'Configurabale'
+	ScopeDes  string // 'Fixed' 'Configurabale'
 	ScopeItem string
 }
 
@@ -259,69 +257,69 @@ type NetworkGateway struct {
 
 // User
 type User struct {
-	Username string
-	Password string
+	Username  string
+	Password  string
 	UserLevel string // 'Administrator', 'Operator', 'User', 'Anonymous', 'Extended'
 }
 
 // RelayOutput
 type RelayOutputSettings struct {
-	Mode string // 'Bistable', 'Monostable'
+	Mode      string // 'Bistable', 'Monostable'
 	DelayTime string
 	IdleState string // 'closed', 'open'
 }
 
 type RelayOutput struct {
-	Token string
+	Token      string
 	Properties RelayOutputSettings
 }
 
 //NetworkZeroConfiguration
 type NetworkZeroConfiguration struct {
 	InterfaceToken string
-	Enabled bool
-	Addresses []string
+	Enabled        bool
+	Addresses      []string
 }
 
 // Service Device
 type DeviceSecurityCapabilitiesService struct {
-	RemoteUserHandling bool
-	Dot1X bool
-	AccesssPolicyConfig bool
+	RemoteUserHandling   bool
+	Dot1X                bool
+	AccesssPolicyConfig  bool
 	OnboardKeyGeneration bool
-	HttpDigest bool
-	X509Token bool
-	DefaultAccessPolicy bool
-	RELToken bool
-	KerberosToken bool
-	TLS12 bool
-	TLS11 bool
-	TLS10 bool
-	UsernameToken bool
-	SAMLToken bool
+	HttpDigest           bool
+	X509Token            bool
+	DefaultAccessPolicy  bool
+	RELToken             bool
+	KerberosToken        bool
+	TLS12                bool
+	TLS11                bool
+	TLS10                bool
+	UsernameToken        bool
+	SAMLToken            bool
 }
 
 type DeviceSystemCapabilitiesService struct {
-	DiscoveryBye bool
+	DiscoveryBye     bool
 	DiscoveryResolve bool
-	FirmwareUpgrade bool
-	SystemLogging bool
-	SystemBackup bool
-	RemoteDiscovery bool
+	FirmwareUpgrade  bool
+	SystemLogging    bool
+	SystemBackup     bool
+	RemoteDiscovery  bool
 }
 
 type DeviceNetworkCapabilitiesService struct {
-	NTP int
-	DynDNS bool
-	IPVersion6 bool
+	NTP               int
+	DynDNS            bool
+	IPVersion6        bool
 	ZeroConfiguration bool
-	IPFilter bool
+	IPFilter          bool
 }
 
 type DeviceCapabilitiesService struct {
 	Security DeviceSecurityCapabilitiesService
-	System DeviceSystemCapabilitiesService
-	Network DeviceNetworkCapabilitiesService
+	System   DeviceSystemCapabilitiesService
+	Network  DeviceNetworkCapabilitiesService
 }
 
 // Service Media
@@ -329,30 +327,30 @@ type MediaProfileCapabilitiesService struct {
 	MaximumNumberOfProfiles int
 }
 type MediaStreamingCapabilitiesService struct {
-	RTP_RTSP_TCP bool
-	RTP_TCP bool
-	RTPMulticast bool
-	NoRTSPStreaming bool
+	RTP_RTSP_TCP        bool
+	RTP_TCP             bool
+	RTPMulticast        bool
+	NoRTSPStreaming     bool
 	NonAggregateControl bool
 }
 
 type MediaCapabilitiesService struct {
-	OSD bool
-	VideoSourceMode bool
-	Rotation bool
-	SnapshotUri bool
-	ProfileCapabilities MediaProfileCapabilitiesService
+	OSD                   bool
+	VideoSourceMode       bool
+	Rotation              bool
+	SnapshotUri           bool
+	ProfileCapabilities   MediaProfileCapabilitiesService
 	StreamingCapabilities MediaStreamingCapabilitiesService
 }
 
 // Service Events
 type EventsCapabilitiesService struct {
-	MaxNotificationProducers int
+	MaxNotificationProducers                      int
 	WSPausableSubscriptionManagerInterfaceSupport bool
-	WSPullPointSupport bool
-	WSSubscriptionPolicySupport bool
-	PersistentNotificationStorage bool
-	MaxPullPoints int
+	WSPullPointSupport                            bool
+	WSSubscriptionPolicySupport                   bool
+	PersistentNotificationStorage                 bool
+	MaxPullPoints                                 int
 }
 
 // Service Imaging
@@ -363,8 +361,8 @@ type ImagingCapabilitiesService struct {
 // Service PTZ
 type PTZCapabilitiesService struct {
 	GetCompatibleConfigurations bool
-	Reverse bool
-	EFlip bool
+	Reverse                     bool
+	EFlip                       bool
 }
 
 type OnvifVersion struct {
@@ -373,17 +371,16 @@ type OnvifVersion struct {
 }
 
 type CapabilitiesService struct {
-	Name string // PTZ, media, device, imaging, events
+	Name         string // PTZ, media, device, imaging, events
 	Capabilities interface{}
 }
 
 type Service struct {
-	Namespace string
-	XAddr string
+	Namespace    string
+	XAddr        string
 	Capabilities CapabilitiesService
-	Version OnvifVersion
+	Version      OnvifVersion
 }
-
 
 // VideoEncoderConfigurationOptions
 type IntRange struct {
@@ -391,160 +388,159 @@ type IntRange struct {
 	Max int
 }
 
-
 type H264Options struct {
-	ResolutionsAvailable MediaBounds
-	GovLengthRange IntRange
-	FrameRateRange IntRange
+	ResolutionsAvailable  MediaBounds
+	GovLengthRange        IntRange
+	FrameRateRange        IntRange
 	EncodingIntervalRange IntRange
 	H264ProfilesSupported []string // 'Baseline', 'Main', 'Extended', 'High'
 }
 
 type VideoEncoderConfigurationOptions struct {
 	QualityRange IntRange
-	H264 H264Options
+	H264         H264Options
 }
 
 // GuaranteedNumberOfVideoEncoderInstances
 type GuaranteedNumberOfVideoEncoderInstances struct {
 	TotalNumber int
-	H264 int
+	H264        int
 }
 
 // VideoSource
 type VideoSource struct {
-	Token string
-	Framerate float64
+	Token      string
+	Framerate  float64
 	Resolution MediaBounds
-	Imaging ImagingSettings
+	Imaging    ImagingSettings
 }
 
 type ImagingSettings struct {
 	BacklightCompensation BacklightCompensation
-	Brightness float64
-	ColorSaturation float64
-	Contrast float64
-	Exposure Exposure
-	Focus FocusConfiguration
-	IrCutFilter string //  'ON', 'OFF', 'AUTO'
-	Sharpness float64
-	WideDynamicRange WideDynamicRange
-	WhiteBalance WhiteBalance
+	Brightness            float64
+	ColorSaturation       float64
+	Contrast              float64
+	Exposure              Exposure
+	Focus                 FocusConfiguration
+	IrCutFilter           string //  'ON', 'OFF', 'AUTO'
+	Sharpness             float64
+	WideDynamicRange      WideDynamicRange
+	WhiteBalance          WhiteBalance
 }
 
 type BacklightCompensation struct {
-	Mode string // 'ON' 'OFF'
+	Mode  string // 'ON' 'OFF'
 	Level float64
 }
 
 type Exposure struct {
-	Mode     string // 'AUTO', 'MANUAL'
-	Priority string //  'LowNoise', 'FrameRate'
-	Window   Rectangle
+	Mode            string // 'AUTO', 'MANUAL'
+	Priority        string //  'LowNoise', 'FrameRate'
+	Window          Rectangle
 	MinExposureTime float64
 	MaxExposureTime float64
-	MinGain float64
-	MaxGain float64
-	MinIris float64
-	MaxIris float64
-	ExposureTime float64
-	Gain float64
-	Iris float64
+	MinGain         float64
+	MaxGain         float64
+	MinIris         float64
+	MaxIris         float64
+	ExposureTime    float64
+	Gain            float64
+	Iris            float64
 }
 
 type Rectangle struct {
-	Top int
+	Top    int
 	Bottom int
-	Left int
-	Right int
+	Left   int
+	Right  int
 }
 
 type FocusConfiguration struct {
 	AutoFocusMode string //  'AUTO', 'MANUAL'
-	DefaultSpeed float64
-	NearLimit float64
-	FarLimit float64
+	DefaultSpeed  float64
+	NearLimit     float64
+	FarLimit      float64
 }
 
 type WideDynamicRange struct {
-	Mode string //  'OFF', 'ON'
+	Mode  string //  'OFF', 'ON'
 	Level float64
 }
 
 type WhiteBalance struct {
-	Mode string //  'AUTO', 'MANUAL'
+	Mode   string //  'AUTO', 'MANUAL'
 	CrGain float64
 	CbGain float64
 }
 
 // Video Source Configuration
 type VideoSourceConfiguration struct {
-	Token string
-	Name string
+	Token       string
+	Name        string
 	SourceToken string
-	Bounds IntRectangle
+	Bounds      IntRectangle
 }
 
 type IntRectangle struct {
-	X int
-	Y int
-	Width int
+	X      int
+	Y      int
+	Width  int
 	Height int
 }
 
 type IntRectangleRange struct {
-	XRange IntRange
-	YRange IntRange
-	WidthRange IntRange
+	XRange      IntRange
+	YRange      IntRange
+	WidthRange  IntRange
 	HeightRange IntRange
 }
 
 type VideoSourceConfigurationOption struct {
-	MaximumNumberOfProfiles int
-	BoundsRange IntRectangleRange
+	MaximumNumberOfProfiles    int
+	BoundsRange                IntRectangleRange
 	VideoSourceTokensAvailable string
 }
 
 type Multicast struct {
-	Address IPAddress
-	Port int
-	TTL int
+	Address   IPAddress
+	Port      int
+	TTL       int
 	AutoStart bool
 }
 
 type MetadataConfiguration struct {
-	Token string
-	Name string
+	Token          string
+	Name           string
 	SessionTimeout string
-	Multicast Multicast
+	Multicast      Multicast
 }
 
 type PTZStatusFilterOptions struct {
-	PanTiltStatusSupported bool
-	ZoomStatusSupported bool
+	PanTiltStatusSupported   bool
+	ZoomStatusSupported      bool
 	PanTiltPositionSupported bool
-	ZoomPositionSupported bool
+	ZoomPositionSupported    bool
 }
 
 type MetadataConfigurationOptions struct {
-	GeoLocation bool
+	GeoLocation            bool
 	PTZStatusFilterOptions PTZStatusFilterOptions
 }
 
 type AudioSource struct {
-	Token string
+	Token    string
 	Channels int //1: mono, 2: stereo
 }
 
 type AudioSourceConfiguration struct {
-	Token string
-	Name string
+	Token       string
+	Name        string
 	SourceToken string
 }
 
 type AudioEncoderConfigurationOption struct {
-	Encoding string // 'G711', 'G726', 'AAC'
-	BitrateList int
+	Encoding       string // 'G711', 'G726', 'AAC'
+	BitrateList    int
 	SampleRateList int
 }
 
@@ -554,51 +550,51 @@ type FloatRange struct {
 }
 
 type Space1DDescription struct {
-	URI string
+	URI    string
 	XRange FloatRange
 }
 
 type Space2DDescription struct {
-	URI string
+	URI    string
 	XRange FloatRange
 	YRange FloatRange
 }
 
 type PTZSpaces struct {
-	AbsolutePanTiltPositionSpace Space2DDescription
-	AbsoluteZoomPositionSpace Space1DDescription
+	AbsolutePanTiltPositionSpace    Space2DDescription
+	AbsoluteZoomPositionSpace       Space1DDescription
 	RelativePanTiltTranslationSpace Space2DDescription
-	RelativeZoomTranslationSpace Space1DDescription
-	ContinuousPanTiltVelocitySpace Space2DDescription
-	ContinuousZoomVelocitySpace Space1DDescription
-	PanTiltSpeedSpace Space1DDescription
-	ZoomSpeedSpace Space1DDescription
+	RelativeZoomTranslationSpace    Space1DDescription
+	ContinuousPanTiltVelocitySpace  Space2DDescription
+	ContinuousZoomVelocitySpace     Space1DDescription
+	PanTiltSpeedSpace               Space1DDescription
+	ZoomSpeedSpace                  Space1DDescription
 }
 
 type PTZNode struct {
-	Token string
-	FixedHomePosition bool
-	GeoMove bool
-	Name string
-	SupportedPTZSpaces PTZSpaces
+	Token                  string
+	FixedHomePosition      bool
+	GeoMove                bool
+	Name                   string
+	SupportedPTZSpaces     PTZSpaces
 	MaximumNumberOfPresets int
-	HomeSupported bool
+	HomeSupported          bool
 }
 
 type Vector2D struct {
 	Space string
-	X float64
-	Y float64
+	X     float64
+	Y     float64
 }
 
 type Vector1D struct {
 	Space string
-	X float64
+	X     float64
 }
 
 type PTZVector struct {
 	PanTilt Vector2D
-	Zoom Vector1D
+	Zoom    Vector1D
 }
 
 type PanTiltLimits struct {
@@ -610,22 +606,22 @@ type ZoomLimits struct {
 }
 
 type PTZConfiguration struct {
-	Token string
-	Name string
-	MoveRamp int
-	PresetRamp int
-	PresetTourRamp int
-	NodeToken string
-	DefaultAbsolutePantTiltPositionSpace string
-	DefaultAbsoluteZoomPositionSpace string
+	Token                                  string
+	Name                                   string
+	MoveRamp                               int
+	PresetRamp                             int
+	PresetTourRamp                         int
+	NodeToken                              string
+	DefaultAbsolutePantTiltPositionSpace   string
+	DefaultAbsoluteZoomPositionSpace       string
 	DefaultRelativePanTiltTranslationSpace string
-	DefaultRelativeZoomTranslationSpace string
-	DefaultContinuousPanTiltVelocitySpace string
-	DefaultContinuousZoomVelocitySpace string
-	DefaultPTZSpeed PTZVector
-	DefaultPTZTimeout string
-	PanTiltLimits PanTiltLimits
-	ZoomLimits ZoomLimits
+	DefaultRelativeZoomTranslationSpace    string
+	DefaultContinuousPanTiltVelocitySpace  string
+	DefaultContinuousZoomVelocitySpace     string
+	DefaultPTZSpeed                        PTZVector
+	DefaultPTZTimeout                      string
+	PanTiltLimits                          PanTiltLimits
+	ZoomLimits                             ZoomLimits
 }
 
 type Options struct {
@@ -633,7 +629,7 @@ type Options struct {
 }
 
 type PTControlDirection struct {
-	EFlip Options // 'OFF', 'ON', 'Extended'
+	EFlip   Options // 'OFF', 'ON', 'Extended'
 	Reverse Options // 'OFF', 'ON', 'AUTO', 'Extended'
 }
 
@@ -643,24 +639,41 @@ type DurationRange struct {
 }
 
 type PTZConfigurationOptions struct {
-	Spaces PTZSpaces
-	PTZTimeout DurationRange
+	Spaces             PTZSpaces
+	PTZTimeout         DurationRange
 	PTControlDirection PTControlDirection
 }
 
 type MoveStatus struct {
 	PanTilt string
-	Zoom string
+	Zoom    string
 }
 
 type PTZStatus struct {
-	Position PTZVector
+	Position   PTZVector
 	MoveStatus MoveStatus
-	UtcTime string
+	UtcTime    string
 }
 
 type PTZPreset struct {
-	Token string
-	Name string
+	Token       string
+	Name        string
 	PTZPosition PTZVector
+}
+
+type SubscriptionReference struct {
+	Address string
+}
+
+type CreatePullPointSubscriptionResponse struct {
+	SubscriptionReference SubscriptionReference
+	CurrentTime           string
+	TerminationTime       string
+}
+
+type NotificationMessage struct {
+	Topic   string
+	UtcTime string
+	Data    string
+	Source  string
 }
